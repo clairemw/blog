0257d8cd629961a6639b54136e6d0290d0a29ff0
Claire Wilson <wilson.m.claire@gmail.com>
2017-08-02 14:32:43 -0400

    commiting steef

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1644tm)8k(q81(a4b0@fntsay8&0musos1n7)ibt8!qlw6g9b('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'c19cw1.pythonanywhere.com']
 
 
 # Application definition
